27544b4cb7cda4a5c34fe206206399bde1483563
aqzhol-hub <42280068+AggressiveGhost@users.noreply.github.com>
2020-11-23 00:17:13 +0600

    jetstream added

--- deans/config/database.php
+++ deans/config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('jdbc:postgresql://hattie.db.elephantsql.com:5432/sfzpzsyq'),
+            'host' => env('DB_HOST', 'hattie.db.elephantsql.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sfzpzsyq'),
+            'username' => env('DB_USERNAME', 'sfzpzsyq'),
+            'password' => env('DB_PASSWORD', 'RuPFHlCYDdgB8ctV87nnFkqdxWWcz1pX'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
